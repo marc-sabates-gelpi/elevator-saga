8a088956af27ee59ab5384f43a313056a611086e
Marc Sabates Gelpi <marc.sabates.gelpi@protonmail.com>
2018-07-07 20:05:57 +0100

    It won't work; dir is always the last action, not the current one

--- elevator-saga.js
+++ elevator-saga.js
@@ -3,32 +3,72 @@
         var getRandomInt = function (max) {
             return Math.floor(Math.random() * Math.floor(max));
         }
-        , sort = function (elevator) {
-            elevator.destinationQueue = elevator.destinationQueue.sort().filter(function(item, pos, ary) {
+        , add&sort = function (params) {
+	    var queue = params["queue"] || [],
+		elevator = params["elevator"] || {},
+		insideService = params["insideService"] || false,
+		addFloorNum = params["addFloorNum"] || 0,
+		intercepting = false;
+
+	    if("down" == elevator.dir()){
+		intercepting = elevator.currentFloor() >= addFloorNum;
+	    } else {
+		intercepting = elevator.currentFloor() <= addFloorNum;
+	    }
+	    
+	    if (intercepting) {
+		queue.push(addFloorNum);
+	    }
+	    
+	    queue.sort().filter(function(item, pos, ary) {
                 return !pos || item != ary[pos - 1];
             });
-            if(elevator.goingDownIndicator()) {
+	    
+            if("down" == elevator.dir()) {
                 elevator.destinationQueue.reverse();
             }
+
+	    if (!intercepting) {
+		queue.push(addFloorNum);
+	    }
             elevator.checkDestinationQueue();
         }
 	, go = function (elevator, floorNum, insideService) {
-            var jump = insideService || false;
+            var insideService = insideService || false
+	    , queue = Array.from(elevator.destinationQueue);
+
+	    elevator.idle = false;
 	    
-            elevator.goToFloor(floorNum, jump);
-            elevator.idle = false;
-	    
-            if(insideService) {
-                sort(elevator);
-            }
+	    elevator.destinationQueue = add&sort({"queue": queue, "elevator": elevator, "insideService": insideService, "addFloorNum": floorNum});
+	    elevator.checkDestionationQueue();
         }
-	, findIdle = function (elevators) {
-            for(let current of elevators) {
-                if (current.idle) {
-                    return current;
-                }
-            }
-            return elevators[getRandomInt(elevators.length)];
+	, findIntercepting = function (params) {
+	    var elevator = params["elevator"] || {},
+		floorNum = params["floorNum"];
+	    
+            if (floorNum) {
+		for (let current of elevators) {
+		    if(("down" == current.dir && current.currentFloor() >= floorNum) or ("up" == current.dir && current.currentFloor() <= floorNum)) {
+			return current;
+		    }
+		}
+	    }
+
+	    for (let current of elevators) {
+		if (current.idle) {
+		    return current;
+		}
+	    }
+
+	    var distance = 999999, closerInReturnTrip = elevators[getRandomInt(elevators.length)];
+	    for (let current of elevators) {
+		var currentDistance = Math.abs(current.currentFloor() - floorNum);
+		if(currentdistance < distance) {
+		    distance = currentDistance;
+		    closerInReturnTrip = current;
+		}
+	    }
+	    return closerInReturnTrip;
         };
 	
         //init elevators
@@ -37,8 +77,22 @@
                 .on("idle", function () {
                     this.idle = true;
                     this.goToFloor(0);
-		}).on("floor_button_pressed", function(floorNum) {
+		}).on("floor_button_pressed", function (floorNum) {
                     go(this, floorNum, true);
+		}).on("stopped_at_floor", function () {
+		    var nextFloor = this.destinationQueue[0],
+			currentFloor = this.currentFloor,
+			dir = "stopped";
+		    
+		    if (nextFloor) {
+			if(currentFloor > nextFloor) {
+			    dir = "down";
+			} else if (currentFloor < nextFloor ) {
+			    dir = "up";
+			}
+		    }
+
+		    this.dir = dir;
 		});
         }
 	
@@ -46,10 +100,10 @@
         for(let current of floors) {
             current
                 .on("up_button_pressed", function () {
-                    go(findIdle(elevators), this.floorNum());
+                    go(findIntercepting({"elevators": elevators, "floorNum": this.floorNum()}), this.floorNum());
 		})
                 .on("down_button_pressed", function () {
-                    go(findIdle(elevators), this.floorNum());
+                    go(findIntercepting({"elevators": elevators, "floorNum": this.floorNum()}), this.floorNum());
 		});
         }
     }
